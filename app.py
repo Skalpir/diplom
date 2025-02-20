c24b9edd3777145a17ebc05d46a4fa8bf92d3215
Skalpir <andryxa1049@gmail.com>
2025-02-20 21:47:57 +0200

    start

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
-#app.py
+from flask import Flask, jsonify
+from functions import *
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/welcome', methods=['GET'])
+def welcome():
+    return 'Система відстеження помилок у датчиках'
+
+@app.route('/sensor-data', methods=['GET'])
+def sensor_data():
+    return jsonify(get_sensor_data())
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ functions.py
@@ -0,0 +1,8 @@
+def get_sensor_data():
+    return {
+        "sensor": "accelerometer",
+        "timestamp": "2025-02-20T12:00:00",
+        "x": 0.12,
+        "y": -0.98,
+        "z": 9.81
+    }
